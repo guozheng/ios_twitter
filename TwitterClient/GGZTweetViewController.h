57d23c9e07cc62684a813f2d403977b1787d6dce
Guozheng Ge <gzge@yahoo-inc.com>
2014-07-13 17:51:32 -0700

    reply, retweet, fav enabled in the tweet detail view

--- TwitterClient/GGZTweetViewController.h
+++ TwitterClient/GGZTweetViewController.h
@@ -17,9 +17,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *time;
 @property (weak, nonatomic) IBOutlet UILabel *retweetCount;
 @property (weak, nonatomic) IBOutlet UILabel *favoriateCount;
-@property (weak, nonatomic) IBOutlet UIImageView *replyImage;
-@property (weak, nonatomic) IBOutlet UIImageView *retweetImage;
-@property (weak, nonatomic) IBOutlet UIImageView *favoriateImage;
+
+@property (weak, nonatomic) IBOutlet UIButton *replyButton;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *favButton;
+
+- (IBAction)onReply:(id)sender;
+- (IBAction)onRetweet:(id)sender;
+- (IBAction)onFav:(id)sender;
 
 @property (nonatomic, strong) NSDictionary *tweet;
 
--- TwitterClient/GGZTweetViewController.m
+++ TwitterClient/GGZTweetViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "GGZTweetViewController.h"
+#import "GGZComposeViewController.h"
+#import "TwitterClient.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface GGZTweetViewController ()
 
-- (void)onReply;
+- (void)reply;
 
 @end
 
@@ -42,7 +44,7 @@
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
     
     // reply button
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reply" style:UIBarButtonItemStylePlain target:self action:@selector(onReply)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Reply" style:UIBarButtonItemStylePlain target:self action:@selector(reply)];
     
     NSDictionary *user = self.tweet[@"user"];
     
@@ -55,10 +57,20 @@
     self.icon.layer.masksToBounds = YES;
     [self.icon setImageWithURL:imageURL placeholderImage:placeholderImage];
     
-    // small icons
-    [self.replyImage setImage:[UIImage imageNamed:@"Reply"]];
-    [self.retweetImage setImage:[UIImage imageNamed:@"Retweet"]];
-    [self.favoriateImage setImage:[UIImage imageNamed:@"Fav"]];
+    // buttons
+    [self.replyButton setImage:[UIImage imageNamed:@"Reply"] forState:UIControlStateNormal];
+    self.replyButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
+    self.replyButton.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
+    
+    NSString *retweetImageName = ([self.tweet[@"retweeted"] boolValue]) ? @"RetweetOn" : @"Retweet";
+    [self.retweetButton setImage:[UIImage imageNamed:retweetImageName] forState:UIControlStateNormal];
+    self.retweetButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
+    self.retweetButton.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
+    
+    NSString *favImageName = ([self.tweet[@"favorited"] boolValue]) ? @"FavOn" : @"Fav";
+    [self.favButton setImage:[UIImage imageNamed:favImageName] forState:UIControlStateNormal];
+    self.favButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
+    self.favButton.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
     
     // name
     self.name.text = user[@"name"];
@@ -87,9 +99,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)onReply
-{
-    NSLog(@"Replying for the tweet: %@", self.tweet[@"id"]);
+- (void)reply {
+    GGZComposeViewController *replyVC = [[GGZComposeViewController alloc] init];
+    replyVC.tweet = self.tweet;
+    [self.navigationController pushViewController:replyVC animated:YES];
+}
+
+- (IBAction)onReply:(id)sender {
+    NSLog(@"reply button pressed");
+    [self reply];
+}
+
+- (IBAction)onRetweet:(id)sender {
+    NSLog(@"retweet button pressed");
+    [[TwitterClient instance] retweetWithStatusId:self.tweet[@"id"] success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *retweetImageName = ([self.tweet[@"retweeted"] boolValue]) ? @"Retweet" : @"RetweetOn";
+        [self.retweetButton setImage:[UIImage imageNamed:retweetImageName] forState:UIControlStateNormal];
+        self.retweetButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
+        self.retweetButton.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
+        NSLog(@"successfully retweeted");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"failed to retweet: %@", error);
+    }];
+}
+
+- (IBAction)onFav:(id)sender {
+    NSLog(@"fav button pressed");
+    [[TwitterClient instance] favoriteWithStatusId:self.tweet[@"id"] success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // switch the image status
+        NSString *favImageName = ([self.tweet[@"favorited"] boolValue]) ? @"Fav" : @"FavOn";
+        [self.favButton setImage:[UIImage imageNamed:favImageName] forState:UIControlStateNormal];
+        self.favButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
+        self.favButton.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
+        NSLog(@"successfully faved tweet");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"failed to fav tweet: %@", error);
+    }];
 }
 
 @end
